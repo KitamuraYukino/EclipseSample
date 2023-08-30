5e2fa0c6a08ae55963e4f900303373a832c251d3
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-08-30 22:36:49 +0900

    firstcommit

--- sample/src/sample/first.java
+++ sample/src/sample/first.java
@@ -4,6 +4,7 @@ public class first {
 
 	public static void main(String[] args) {
 		System.out.println("(1)Eclipseを始める");
+		System.out.println("(2)"+10*10*5);
 
 	}
 
---
+++ sample_second/src/sample_second/second.java
@@ -0,0 +1,10 @@
+package sample_second;
+
+public class second {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
