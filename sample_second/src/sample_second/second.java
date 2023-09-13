b3889fdffc371496988539cb6ce44a1b5266418b
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-09-13 20:49:31 +0900

    課題提出

--- sample_second/src/sample_second/second.java
+++ sample_second/src/sample_second/second.java
@@ -3,7 +3,34 @@ package sample_second;
 public class second {
 
 	public static void main(String[] args) {
+		//practice
+		int month;
+		month = 9;
+		int day = 13;
 		
+		System.out.println("本日は");
+		System.out.println(month + "月" + day + "日です。");
+		
+		//[課題１]
+		int x = 7;
+		
+		x = x * 3;
+		System.out.println(x);
+		
+		x = x / 2;
+		System.out.println(x);
+		
+		//[課題２]
+		int X = 19;
+		int y = 23;
+		int z = X * y;
+		
+		System.out.println(z);
+		
+		//[課題３]
+		String s = "北村ゆき乃";
+		int x_ = 24;
+		System.out.println(s + "は" + x_ + "歳です。");
 
 	}
 
