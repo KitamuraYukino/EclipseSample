ba92dc88c55b427e6b9aca139393517d61170868
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-09-21 07:24:40 +0900

    switchサンプルコード

--- Inputting_data/src/inputting_data/inputting_data.java
+++ Inputting_data/src/inputting_data/inputting_data.java
@@ -3,9 +3,9 @@ package inputting_data;
 public class inputting_data {
 
 	public static void main(String[] args) {
+		//[データの入力と条件分岐]
 		//サンプルコード
-		//int price = Integer.parseInt(args[0]);
-		int price = 5000;
+		int price = Integer.parseInt(args[0]);
 		double rate = 0.10;
 		int discount, amount;
 		
@@ -20,7 +20,22 @@ public class inputting_data {
 		amount = (int)((price - discount) * (1 + rate));
 		System.out.println("値引金額" + discount + "円");
 		System.out.println("税込金額" + amount + "円");
-		//
+		
+		//[Switch文サンプルコード]
+		int num = Integer.parseInt(args[1]);
+		switch( num ) {
+		case 1:
+			System.out.println("入園料金：8,400円");
+			break;
+		case 2:
+			System.out.println("入園料金：7,000円");
+			break;
+		case 3:
+			System.out.println("入園料金：5,000円");
+			break;
+		default:
+			System.out.println("1:一般, 2:中・高校生, 3:小学生・幼児");
+		}
 	}
 
 }
