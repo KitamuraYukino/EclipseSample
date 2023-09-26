07b5be7787de6ad5972b9376247b1e494b9cffad
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-09-26 20:50:03 +0900

    サンプルコード

--- object/src/object/object.java
+++ object/src/object/object.java
@@ -3,7 +3,18 @@ package object;
 public class object {
 
 	public static void main(String[] args) {
+//		[オブジェクト指向]
+//		[サンプルコード]
 		
+		int[] score1 = {80, 100, 75, 90};
+		int[] score2 = {90, 80, 85, 70};
+		String[] name = {"菅原", "村山", "寺島", "須藤"};
+		String[] sub = {"数学", "英語"};
+		
+		
+		for(int i = 0; i < score.length; i++) {
+			System.out.println(name[i] + "さん：" + score[i] + "点");
+		}
 
 	}
 
