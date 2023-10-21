a0381f54304f3ada0ac1942d13cb1aab08c3d631
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-10-21 23:47:00 +0900

    sample code

--- modifier/src/modifier/Student.java
+++ modifier/src/modifier/Student.java
@@ -1,7 +1,7 @@
 package modifier;
 
 public class Student {
-//設計図の方
+/*設計図の方
 	
 	String name;
 	static int counter = 0;
@@ -14,5 +14,27 @@ public class Student {
 	
 	static void display() {
 		System.out.println(counter + "人です");
+	}
+	
+*/	
+//	カプセル化、アクセス修飾子
+	
+	private String nameA;
+	private int scoreA;
+	
+	public Student(String x) {
+		nameA = x;
+	}
+	public void setScore(int s) {
+		if(0 <= s && s <= 100) {
+			scoreA = s;
+		} else {
+			System.out.println(nameA + "さんの点数が範囲外です");
+			scoreA = 0;
+		}
+	}
+	
+	void display1() {
+		System.out.println(nameA + "さん：" + scoreA + "点");
 	}
 }
--- modifier/src/modifier/modifier.java
+++ modifier/src/modifier/modifier.java
@@ -3,7 +3,7 @@ package modifier;
 public class modifier {
 
 	public static void main(String[] args) {
-//		実行する方
+/*		実行する方
 		Student.display();
 		
 		Student stu1 = new Student("北村");
@@ -11,6 +11,18 @@ public class modifier {
 		
 		Student stu2 = new Student("武内");
 		Student.display();
+*/		
+		
+//		アクセス修飾子
+		Student stu3 = new Student("北村");
+		stu3.setScore(80);
+		stu3.display1();
+		
+		Student stu4 = new Student("武内");
+		stu4.setScore(-50);
+//		stu4.scoreA = -50;
+		stu4.display1();
+		
 
 	}
 
