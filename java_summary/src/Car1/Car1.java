0bbd77c357fcb7bc23200a1d1eba74dff80ccb8d
Kitamura Yukino <ykn.kita5023@gmail.com>
2023-11-12 23:27:02 +0900

    編集中

---
+++ java_summary/src/Car1/Car1.java
@@ -0,0 +1,25 @@
+package Car1;
+
+class Car1{
+  int no;
+  int speed;
+
+  void setNo(int n){
+    no = n;
+  }
+  void run(int s){
+    speed = s;
+  }
+  // brake
+  void brake() {
+	  speed = 0;
+  }
+  
+  void brake(int s) {
+	  speed = speed - s;
+  }
+
+  void display(){
+	  System.out.println("ナンバー" + no + "の速度は" + speed + "です");
+  }
+}
--- java_summary/src/Car1/DriveCar1.java
+++ java_summary/src/Car1/DriveCar1.java
@@ -1,3 +1,4 @@
+package Car1;
 class DriveCar1{
   public static void main(String[] args){
     Car1 c1 = new Car1();
